836270b6faea49435729272c3840923c0421ea32
Nico de Boer <nico@diverse-it.nl>
2026-08-29 15:16:46 +0200

    Add GIF support for squats, lunges, and reverse lunges animations
    Switch ExerciseAnimation component to render GIFs if available, falling back to Lottie animations otherwise. Add GIF files for squats, lunges, and reverse lunges to enhance animation compatibility.

--- src/ExerciseAnimation.jsx
+++ src/ExerciseAnimation.jsx
@@ -8,23 +8,32 @@ import burpeesData from './animations/burpees.json'
 import splitjumpsData from './animations/splitjumps.json'
 import plankarmraiseData from './animations/plankarmraise.json'
 import reverselungesData from './animations/reverselunges.json'
+import squatsGif from './animations/squats.gif'
+import lungesGif from './animations/lunges.gif'
+import reverselungesGif from './animations/reverselunges.gif'
 
 const LOTTIE_DATA = {
   pushups: pushupsData,
-  squats: squatsData,
   plank: plankData,
-  lunges: lungesData,
   burpees: burpeesData,
   splitjumps: splitjumpsData,
   plankarmraise: plankarmraiseData,
   squatsreach: squatsReachData,
-  reverselunges: reverselungesData,
+}
+
+const GIF_DATA = {
+  squats: squatsGif,
+  lunges: lungesGif,
+  reverselunges: reverselungesGif,
 }
 
 export function ExerciseAnimation({ id, playing = true, size = 140 }) {
+  const gif = GIF_DATA[id]
   return (
     <div style={{ width: size, height: size, background: '#fff', borderRadius: 10, overflow: 'hidden', flexShrink: 0 }}>
-      <Lottie src={LOTTIE_DATA[id]} autoplay={playing} loop style={{ width: size, height: size }} />
+      {gif
+        ? <img src={gif} alt="" style={{ width: size, height: size, objectFit: 'cover' }} />
+        : <Lottie src={LOTTIE_DATA[id]} autoplay={playing} loop style={{ width: size, height: size }} />}
     </div>
   )
 }
