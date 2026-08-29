45ee27c241f23731791eb20d68b6fdbd9829199b
Nico de Boer <nico@diverse-it.nl>
2026-08-29 14:49:08 +0200

    Update lunges.json animation with refined motion paths and layer adjustments

--- src/ExerciseAnimation.jsx
+++ src/ExerciseAnimation.jsx
@@ -1,11 +1,13 @@
 import { Lottie } from 'lottie-react'
 import pushupsData from './animations/pushups.json'
 import squatsData from './animations/squats.json'
+import squatsReachData from './animations/squatsreach.json'
 import plankData from './animations/plank.json'
 import lungesData from './animations/lunges.json'
 import burpeesData from './animations/burpees.json'
 import splitjumpsData from './animations/splitjumps.json'
 import plankarmraiseData from './animations/plankarmraise.json'
+import reverselungesData from './animations/reverselunges.json'
 
 const LOTTIE_DATA = {
   pushups: pushupsData,
@@ -15,6 +17,8 @@ const LOTTIE_DATA = {
   burpees: burpeesData,
   splitjumps: splitjumpsData,
   plankarmraise: plankarmraiseData,
+  squatsreach: squatsReachData,
+  reverselunges: reverselungesData,
 }
 
 export function ExerciseAnimation({ id, playing = true, size = 140 }) {
--- src/data.js
+++ src/data.js
@@ -1,11 +1,13 @@
 export const EXERCISES = [
-  { id: 'pushups', name: 'Push-ups',              duration: 30 },
-  { id: 'squats',  name: 'Squat Reach',           duration: 30 },
-  { id: 'plank',   name: 'Plank',                 duration: 40 },
-  // { id: 'lunges',  name: 'Reverse Lunges',        duration: 30 },
-  { id: 'burpees', name: 'Burpees',               duration: 30 },
-  { id: 'splitjumps', name: 'Split Jumps',        duration: 30 },
+  { id: 'pushups',       name: 'Push-ups',        duration: 30 },
+  { id: 'squats',        name: 'Squats',          duration: 30 },
+  { id: 'plank',         name: 'Plank',           duration: 40 },
+  { id: 'lunges',        name: 'Lunges',          duration: 30 },
+  { id: 'burpees',       name: 'Burpees',         duration: 30 },
+  { id: 'splitjumps',    name: 'Split Jumps',     duration: 30 },
   { id: 'plankarmraise', name: 'Plank Arm Raise', duration: 40 },
+  { id: 'squatsreach',   name: 'Squat Reach',     duration: 30 },
+  { id: 'reverselunges', name: 'Reverse Lunges',  duration: 30 },
 ]
 
 export const ROUNDS = 3
